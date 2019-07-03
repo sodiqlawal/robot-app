9164db830aa00cb5c8efbe05afb7e8b61fbb2ede
Sodiq <sodiqlawal95@gmail.com>
2019-07-03 13:42:12 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,82 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne Graham',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 2,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'Shanna@melissa.tv'
+},
+{
+	id: 3,
+	name: 'Clementine Bauch',
+	username: 'Samantha',
+	email: 'Nathan@yesenia.net'
+},
+{
+	id: 4,
+	name: 'Patricia Lebsack',
+	username: 'Karianne',
+	email: 'Julianne.Oconner@kory.org'
+},
+{
+	id: 5,
+	name: 'Chelsey Dietrich',
+	username: 'Kamren',
+	email: 'Lucio_Hettinger@annie.ca'
+},
+{
+	id: 6,
+	name: 'Mrs. Dennis Schulist',
+	username: 'Leopoldo_Corkery',
+	email: 'Karley_Dach@jasper.info'
+},
+{
+	id: 7,
+	name: 'Kurtis Weissnat',
+	username: 'Elwyn.Skiles',
+	email: 'Telly.Hoeger@billy.biz'
+},
+{
+	id: 8,
+	name: 'Nicholas Runolfsdottir v',
+	username: 'Maxime_Nienow',
+	email: 'Sherwood@rosamond.ne'
+},
+{
+	id: 9,
+	name: 'Gienna Reichert',
+	username: 'Delphine',
+	email: 'Chain_McDermott@dana.io'
+},
+{
+	id: 0,
+	name: 'Clementine DuBuque',
+	username: 'Noriah.Stanton',
+	email: 'Rey.Padberg@karina.biz'
+},
+
+
+
+
+
+
+
+
+
+
+]
+
+
+
+
+export default robots;
+
+
+
+
+
