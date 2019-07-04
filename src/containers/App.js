78a55515e8f22a6ed0e06ccd55bafdb30bec8246
Sodiq <sodiqlawal95@gmail.com>
2019-07-04 23:20:00 +0100

    made some few changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,11 +27,10 @@ class App extends Component {
 	} 
 
 	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots =robots.filter(robot =>{
-		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		const filteredRobots = this.state.robots.filter(robots =>{
+		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		});
-		return !robots.length ? <h1>Loading</h1> :
+
 		return (
 			<div className='tc'>
 				<h1 className='f1'>Robofriends</h1>
@@ -40,7 +39,7 @@ class App extends Component {
 				<CardList robots={filteredRobots}/>
 				</Scroll>
 			</div>
-		);	
+		);
 	}
 }
 
